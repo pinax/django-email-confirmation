75b8d1ad36f848a8bd724b2054ca7a9a6f9bcf76
Tom Clancy <tclancy@gmail.com>
2012-02-07 10:54:39 -0500

    Required proper user to confirm email address.

--- emailconfirmation/views.py
+++ emailconfirmation/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render_to_response
+from django.http import HttpResponseForbidden
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 
 from emailconfirmation.models import EmailConfirmation
@@ -7,6 +8,10 @@ from emailconfirmation.models import EmailConfirmation
 def confirm_email(request, confirmation_key):
     
     confirmation_key = confirmation_key.lower()
+    confirmation = get_object_or_404(EmailConfirmation,
+                                confirmation_key=confirmation_key)
+    if confirmation.email_address.user != request.user:
+        raise HttpResponseForbidden
     email_address = EmailConfirmation.objects.confirm_email(confirmation_key)
     
     return render_to_response("emailconfirmation/confirm_email.html", {
