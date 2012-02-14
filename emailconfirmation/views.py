05c00834099f3c5f031d684431962316915a7aaf
Tom Clancy <tclancy@gmail.com>
2012-02-14 09:38:30 -0500

    Modified case where user can't confirm email to show normal error message.

--- emailconfirmation/views.py
+++ emailconfirmation/views.py
@@ -10,9 +10,10 @@ def confirm_email(request, confirmation_key):
     confirmation_key = confirmation_key.lower()
     confirmation = get_object_or_404(EmailConfirmation,
                                 confirmation_key=confirmation_key)
-    if confirmation.email_address.user != request.user:
-        return HttpResponseForbidden("You cannot confirm that email address")
-    email_address = EmailConfirmation.objects.confirm_email(confirmation_key)
+    if confirmation.email_address.user == request.user:
+        email_address = EmailConfirmation.objects.confirm_email(confirmation_key)
+    else:
+        email_address = None
     
     return render_to_response("emailconfirmation/confirm_email.html", {
         "email_address": email_address,
